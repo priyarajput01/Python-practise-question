53fed581b0fac7563e996d77f096b8265406f900
priyarajput01 <133469609+priyarajput01@users.noreply.github.com>
2025-03-11 12:06:40 +0530

    Add files via upload

---
+++ Datastrctureques.py
@@ -0,0 +1,20 @@
+# Exercise 1: Create a list by picking an odd-index items from the first list and even index items from the second
+# Given two lists, l1 and l2, write a program to create a third list l3 by picking an odd-index element from the list l1 and even index elements from the list l2.
+# 
+
+a = 2
+b = 5
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+        
+        
+a = 100
+b = 200
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")  
+else:
+    print("a is greater than b")              
